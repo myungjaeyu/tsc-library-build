d6567a6de25e21169eafd71b2dd77fcd27565fad
u4bi <admin@naver.id>
2018-02-20 16:20:39 +0900

    Added render methods to App.

--- src/todo/App.ts
+++ src/todo/App.ts
@@ -4,11 +4,15 @@ namespace todo
     export class APP
     {
         private _service : IService;
+        private _renderer : Renderer;
 
 
-        constructor(items : string[])
+        constructor(element : HTMLElement, items : string[])
         {
+            this._renderer = new Renderer(element);
             this._service = new Service(items);
+
+            this._initialize(element);
         }
 
 
@@ -21,18 +25,38 @@ namespace todo
         public add(content : string) : void
         {
             this._service.add(content);
+            this.render();
         }
 
 
         public toggle(id : number) : void
         {
             this._service.toggle(id);
+            this.render();
         }
 
 
         public remove(id : number) : void
         {
             this._service.remove(id);
+            this.render();
+        }
+
+
+        public render() : void
+        {
+            this._renderer.render( this._service.getAll() );
+        }
+
+
+        private _initialize(element : HTMLElement) : void
+        {
+
+            element.addEventListener('click', (e) => {
+                if (e.target.parentElement.className === 'Todo__Item')
+                    this[e.target['className'].replace('Item__', '')]( +e.target.parentElement.dataset.itemId );
+            });
+
         }
 
     }
--- src/todo/typings/App.d.ts
+++ src/todo/typings/App.d.ts
@@ -1,10 +1,13 @@
 declare namespace todo {
     class APP {
         private _service : IService;
-        constructor(items: Item[]);
+        private _renderer : Renderer;
+        constructor(element: HTMLElement, items: string[]);
         getAll(): Item[];
         add(item: any): void;
         toggle(id: number): void;
         remove(id: number): void;
+        render(): void;
+        private _initialize(element);
     }
 }
